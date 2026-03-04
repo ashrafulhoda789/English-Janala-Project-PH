c6b769013c3d81f0265190407fcdc6b550c2c574
ashraful-hoda <asrafulhoda789@gmail.com>
2026-03-05 02:42:58 +0600

    add level button api and make display word section designed

--- script/index.js
+++ script/index.js
@@ -8,6 +8,28 @@ const loadLessons = () => {
 // Lessons object
 // {id: 101, level_no: 1, lessonName: 'Basic Vocabulary'}
 
+const loadLevelWord = (id) => {
+    // console.log(id);
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words) =>{
+    const wordContainer = document.getElementById('word-container');
+    // wordContainer.innerHTML = "";
+
+    words.forEach(word => {
+        const card = document.createElement("div");
+        card.innerHTML = `
+            <p>Cat</p>
+        `;
+
+        wordContainer.appendChild(card);
+    })
+}
+
 const displayLessons = (lessons) => {
     
     // 1. get container and make empty
@@ -16,14 +38,15 @@ const displayLessons = (lessons) => {
 
     // get into every lessons
     for(let lesson of lessons){
-        console.log(lesson);
+        // console.log(lesson);
         // 3. create element
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-            <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
+            <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
         `;
         // 4. appent the child
         levelContainer.appendChild(btnDiv);
     }
 }
+
 loadLessons();
